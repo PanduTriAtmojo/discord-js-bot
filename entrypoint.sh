eb0f0e7d9ea13f75ac56bd9cdb70d36841012c32
PanduTriAtmojo <123941242+PanduTriAtmojo@users.noreply.github.com>
2024-06-08 18:46:40 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,7 @@
 # Start Lavalink in the background
 java -jar Lavalink.jar &
 
-sleep 30
-
-if netstat -tuln | grep 2333; then
-  echo "Lavalink is running on port 2333"
-else
-  echo "Lavalink is not running"
-  exit 1
-fi
+sleep 60
 
 # Setelah Lavalink selesai dimulai, mulai aplikasi Node.js
 npm start
