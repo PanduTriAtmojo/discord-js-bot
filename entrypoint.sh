d92c184b8b3e1920ff565f2bbb1038074c8dd63e
PanduTriAtmojo <123941242+PanduTriAtmojo@users.noreply.github.com>
2024-06-08 16:42:35 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,5 @@
 # Start Lavalink in the background
 java -jar Lavalink.jar &
 
-# Wait for Lavalink to start
-sleep 10
-
-# Check if Lavalink port is open
-if netstat -tuln | grep 2333; then
-  echo "Lavalink is running on port 8080"
-else
-  echo "Lavalink is not running"
-  exit 1
-fi
-
 # Start Node.js application
 node index.js
